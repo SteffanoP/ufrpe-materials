06cf1c634217d675b54788cca69190add87d44c8
SteffanoP <steffanoxpereira@gmail.com>
2021-04-09 00:03:16 -0300

    Aula 02.1 - Ambiente Java e Ferramentas

---
+++ 91930-introducao-a-programacao-2/praticas/HelloWorld/com-ide/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //Imprimir String Hello World em java com saída padrão de sistema
+        System.out.println("Hello Java World!");
+    }
+}
